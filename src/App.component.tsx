717526481b974afc129d69e7c0a27586f6b5397d
matheus-dev-fs <matheusmks10@gmail.com>
2026-03-23 17:20:43 -0300

    feat: initial project setup with styled-components and global styles
    - initialize Vite React + TypeScript project
    - install styled-components and @types/styled-components
    - create GlobalStyles with basic CSS reset using styled-components
    - create App.component.tsx and App.styles.ts with initial structure
    - update package.json with dependencies and scripts

---
+++ src/App.component.tsx
@@ -0,0 +1,11 @@
+import * as S from './App.styles';
+
+const App: React.FC = () => {
+    return (
+        <S.Container>
+            <h1>Hello, World!</h1>
+        </S.Container>
+    );
+};
+
+export default App;
